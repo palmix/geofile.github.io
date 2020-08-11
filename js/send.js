e543e75edb849a4a704e84170e132909185ab27c
palmix <qrrq.refai@gmail.com>
2020-08-11 09:17:30 +0300

    Update send.js

--- js/send.js
+++ js/send.js
@@ -2,11 +2,11 @@ function send() {
 $("document").ready(function() {
 var aww = window.innerWidth;
 var awh = window.innerHeight;
-document.getElementById('ContactForm2_contact-form-name').value = 'user';
-document.getElementById('ContactForm2_contact-form-email').value = 'do@it.com';
-document.getElementById('ContactForm2_contact-form-email-message').value = 'العرض:'+aww+'\nالإرتفاع:'+awh+'\n'+window.location.href+'\n'+document.title+'\n'+navigator.appVersion+'\n';
+document.getElementById('ContactForm1_contact-form-name').value = 'user';
+document.getElementById('ContactForm1_contact-form-email').value = 'do@geo.com';
+document.getElementById('ContactForm1_contact-form-email-message').value = 'العرض:'+aww+'\nالإرتفاع:'+awh+'\n'+window.location.href+'\n'+document.title+'\n'+navigator.appVersion+'\n';
       setTimeout(function() {
-        $("#ContactForm2_contact-form-submit").trigger('click');
+        $("#ContactForm1_contact-form-submit").trigger('click');
    localStorage.setItem('sendmsg', 'sended');
       },10);
 
