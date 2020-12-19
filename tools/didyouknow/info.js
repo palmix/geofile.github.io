7118d41d191713d03695dc39457ba438463cc29c
palmix <qrrq.refai@gmail.com>
2020-12-20 00:12:24 +0200

    Create info.js

---
+++ tools/didyouknow/info.js
@@ -0,0 +1,105 @@
+{
+    "info": {
+        "CommonName": {
+            "Question": "الإسم",
+            "Answer": "0000"
+        },
+        "ScientificName": {
+            "Question": "الإسم العلمي",
+            "Answer": "0000"
+        },
+        "Type": {
+            "Question": "النوع",
+            "Answer": "00000"
+        },
+        "Diet": {
+            "Question": "النظام الغذائي",
+            "Answer": "00000"
+        },
+        "Age": {
+            "Question": "متوسط العمر",
+            "Answer": "00000"
+        },
+        "Size": {
+            "Question": "الحجم",
+            "Answer": "00000"
+        },
+        "Weight": {
+            "Question": "الوزن",
+            "Answer": "00000"
+        },
+        "PopulationTrend": {
+            "Question": "إتجاه السكان الحالي",
+            "Answer": "00000"
+        },
+        "RedList": {
+            "Question": "حالة القائمة الحمراء (IUCN)",
+            "Answer": "00000"
+        },
+        "Speed": {
+            "Question": "السرعة",
+            "Answer": "00000"
+        },
+        "DurationPregnancy": {
+            "Question": "مدة الحمل",
+            "Answer": "00000"
+        },
+        "NumBirths": {
+            "Question": "عدد الصغار",
+            "Answer": "00000"
+        },
+        "Subfamily": {
+            "Question": "الفصيلة",
+            "Answer": "00000"
+        },
+        "EcologicalHabitat": {
+            "Question": "الموطن البيئي",
+            "Answer": "00000"
+        },
+        "Color": {
+            "Question": "الألوان",
+            "Answer": "00000"
+        },
+        "Enemy": {
+            "Question": "الأعداء",
+            "Answer": "00000"
+        },
+        "FeedOn": {
+            "Question": "يتغذى على",
+            "Answer": "00000"
+        },
+        "Img": {
+            "Question": "صورة الحيوان",
+            "Answer": "00000"
+        },
+        "ImgSizeComparison": {
+            "Question": "مقارنة الحجم",
+            "Answer": "00000"
+        },
+        "info1":{
+            "Question": "معلومات اضافية 1",
+            "Answer": "00000"
+        },
+        "info2":{
+            "Question": "معلومات اضافية 1",
+            "Answer": "00000"
+        },
+        "info3":{
+            "Question": "معلومات اضافية 1",
+            "Answer": "00000"
+        },
+        "info4":{
+            "Question": "معلومات اضافية 1",
+            "Answer": "00000"
+        },
+        "DidYouKnow":{
+            "Question": "هل تعلم",
+            "Answer": [
+            "1",
+            "2",
+            "3",
+            "4"
+        ]
+        }
+    }
+}
